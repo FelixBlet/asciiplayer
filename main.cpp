c89def3fb6c903d7b7a5b9c5d9f930f4aa3f4dd1
FelixBlet <46885826+FelixBlet@users.noreply.github.com>
2024-10-07 04:32:20 +0500

    lil changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "libavformat/avio.h"
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int main() {
+    const char* url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+    AVIODirContext *ctx = NULL;
+    if (avio_open_dir(&ctx, url, NULL) < 0) {
+        fprintf(stderr, "Cannot open directory.\n");
+        abort();
+    }
     return 0;
 }
